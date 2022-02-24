f63534692d1effd97620d88c0a6961783b8af1b5
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-23 16:07:24 -0800

    changed secrets in github

--- src/services/client.js
+++ src/services/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 export const client = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
 
 export const parseData = ({ data, error }) => {
